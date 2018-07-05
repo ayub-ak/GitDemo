d519b546add35019d94c902feaff296c14e32e0a
ayub-ak <ayub.ak@gmail.com>
2018-07-05 20:30:18 +0530

    Second commit from dev B (second workspace)

--- GitTest/src/main/java/com/GitTest/test/TestClass.java
+++ GitTest/src/main/java/com/GitTest/test/TestClass.java
@@ -3,7 +3,8 @@ package com.GitTest.test;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("Git Test");
+		System.out.println("Git Test to understand how to resolve conflict");
+		System.out.println("New line added");
 
 	}
 
