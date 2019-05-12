a6fc887c91d0626a9e3c4590f83af6c028d55ecb
ayub-ak <ayub.ak@gmail.com>
2019-05-12 11:18:37 +0530

    To test commit and push operation

--- GitTest/src/main/java/com/GitTest/test/TestClass.java
+++ GitTest/src/main/java/com/GitTest/test/TestClass.java
@@ -5,7 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Git Test to understand how to resolve conflict");
 		System.out.println("New line added");
-
+		System.out.println("Adding another test code for commit operation");
 	}
 
 }
